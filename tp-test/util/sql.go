80751493d5ed93e7ded3deb9738f213f00f52605
zyguan <zhongyangguan@gmail.com>
2020-08-24 11:49:30 +0800

    consider explain/describe as a query
    Signed-off-by: zyguan <zhongyangguan@gmail.com>

--- tp-test/util/sql.go
+++ tp-test/util/sql.go
@@ -4,7 +4,7 @@ import "strings"
 
 func IsQuery(sql string) bool {
 	sql = strings.ToLower(strings.TrimSpace(sql))
-	for _, w := range []string{"select ", "show ", "admin show "} {
+	for _, w := range []string{"select ", "show ", "admin show ", "explain ", "desc ", "describe "} {
 		if strings.HasPrefix(sql, w) {
 			return true
 		}
